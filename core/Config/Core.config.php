edbbb0cb7400c90cd693c16d94a5624ada24e8db
Drew Ditthardt <dditthardt@olivinelabs.com>
2011-12-20 21:18:04 -0800

    Fixed issue with constants in config

--- core/Config/Core.config.php
+++ core/Config/Core.config.php
@@ -1,10 +1,6 @@
 <?php
 define('MINT_ROOT', getcwd().'/');
 define('AKISMET_API_KEY', '4b66e21dd53c');
-define('NAMESPACE_CUSTOM', MINT_ROOT.'../../core-custom/');
-define('HANDLER_DIR', MINT_ROOT.'../../handlers/');
-define('VIEW_DIR', MINT_ROOT.'../../views/');
-define('CONFIG_DIR', MINT_ROOT.'../config/');
 $SessionName = 'mint';
 $Timezone = 'America/New_York';
 if(file_exists('../../config/Core.config.php'))
