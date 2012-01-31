cb3b485924b9f0893db9a599fb50bbef7ec5b826
Drew Ditthardt <dditthardt@olivinelabs.com>
2012-01-30 22:59:38 -0800

    FIXED IT FINALLY

--- core/Config/Core.config.php
+++ core/Config/Core.config.php
@@ -1,10 +1,10 @@
 <?php
-$dir = __DIR__;
-if($dir[strlen($dir)-1] != '/' && $dir[strlen($dir)-1] != '\\')
+$dir = str_replace('\\', '/', __DIR__);
+if($dir[strlen($dir)-1] != '/')
 {
   $dir .= '/';
 }
-define('MINT_ROOT', $dir.'../../');
+define('MINT_ROOT', $dir.'../');
 define('AKISMET_API_KEY', '4b66e21dd53c');
 $SessionName = 'mint';
 $Timezone = 'America/New_York';
