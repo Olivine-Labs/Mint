b313a95862037ccf21342b4db13e37f6ae99a409
Drew Ditthardt <dditthardt@olivinelabs.com>
2012-01-30 22:54:12 -0800

    ...

--- core/Config/Core.config.php
+++ core/Config/Core.config.php
@@ -1,5 +1,10 @@
 <?php
-define('MINT_ROOT', __DIR__.'/../../');
+$dir = __DIR__;
+if($str[strlen($dir)-1] != '/' && $str[strlen($dir)-1] != '\\')
+{
+  $dir .= '/';
+}
+define('MINT_ROOT', $dir.'../../');
 define('AKISMET_API_KEY', '4b66e21dd53c');
 $SessionName = 'mint';
 $Timezone = 'America/New_York';
