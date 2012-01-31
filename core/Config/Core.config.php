a931d5584df4fdd39fee2c6ce89cf627845762cd
Drew Ditthardt <dditthardt@olivinelabs.com>
2012-01-30 22:47:08 -0800

    fixed linux and mac MINT_ROOT

--- core/Config/Core.config.php
+++ core/Config/Core.config.php
@@ -1,5 +1,5 @@
 <?php
-define('MINT_ROOT', __DIR__.'../../');
+define('MINT_ROOT', __DIR__.'/../../');
 define('AKISMET_API_KEY', '4b66e21dd53c');
 $SessionName = 'mint';
 $Timezone = 'America/New_York';
