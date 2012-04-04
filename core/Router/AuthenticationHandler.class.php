3a269e44f2f58afd6fd7546f3e3e7fc214df3ae9
Drew Ditthardt <dditthardt@olivinelabs.com>
2012-04-03 23:40:04 -0700

    fixed weird bug with mint

--- core/Router/AuthenticationHandler.class.php
+++ core/Router/AuthenticationHandler.class.php
@@ -42,6 +42,8 @@ abstract class AuthenticationHandler extends \Router\Handler
       else
       {
         $this->_currentUser = $this->_context->CurrentUser;
+        if($this->_currentUser->Id == "")
+          $this->_currentUser->Id = " ";
       }
     }
 
