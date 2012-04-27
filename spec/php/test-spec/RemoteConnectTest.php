e87d261b401959049a6dbbfe3d3b3c528d18e737
Jack Lawson <ajacksified@gmail.com>
2012-04-26 20:36:39 -0700

    Just a test spec

---
+++ spec/php/test-spec/RemoteConnectTest.php
@@ -0,0 +1,15 @@
+<?php
+require_once('RemoteConnect.php');
+class RemoteConnectTest extends PHPUnit_Framework_TestCase
+{
+  public function setUp(){ }
+  public function tearDown(){ }
+  public function testConnectionIsValid()
+  {
+    // test to ensure that the object from an fsockopen is valid
+    $connObj = new RemoteConnect();
+    $serverName = 'www.google.com';
+    $this->assertTrue($connObj->connectToServer($serverName) !== false);
+  }
+}
+?>
